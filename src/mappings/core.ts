be9a618306253fef7aa04492570794a8f3fb9fdd
Kevin Kennis <kevinvkennis@gmail.com>
2021-04-15 19:05:16 -0700

    add skeleton for all handlers

--- src/mappings/core.ts
+++ src/mappings/core.ts
@@ -1,6 +1,12 @@
-import { Swap } from '../types/templates/Pair/Pair'
+import { Swap, Mint as PoolMint, Burn as PoolBurn } from '../types/templates/Pool/Pool'
 import { log } from '@graphprotocol/graph-ts'
 
 export function handleSwap(event: Swap): void {
   log.debug('testing it: {}', [event.params.amount0.toString()])
 }
+
+export function handlePoolMint(event: PoolMint): void {
+}
+
+export function handlePoolBurn(event: PoolBurn): void {
+s}
