a32ac073058f5b8329745b3462216461afee27f2
ianlapham <ianlapham@gmail.com>
2021-05-21 13:35:14 -0400

    remove graft and add hotfix for high volume token

--- src/mappings/core.ts
+++ src/mappings/core.ts
@@ -259,6 +259,12 @@ export function handleSwap(event: SwapEvent): void {
   let bundle = Bundle.load('1')
   let factory = Factory.load(FACTORY_ADDRESS)
   let pool = Pool.load(event.address.toHexString())
+
+  // hot fix for bad pricing
+  if (pool.id == '0x9663f2ca0454accad3e094448ea6f77443880454') {
+    return
+  }
+
   let token0 = Token.load(pool.token0)
   let token1 = Token.load(pool.token1)
 
