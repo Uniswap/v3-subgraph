48c38ce9a3a38e72958381c2bf3dd0a3ec76b464
ianlapham <ianlapham@gmail.com>
2022-03-11 12:56:40 -0500

    fix export style in index

--- src/mappings/core/index.ts
+++ src/mappings/core/index.ts
@@ -2,11 +2,26 @@ import { handleInitialize as handleInitializeHelper } from './initialize'
 import { handleMint as handleMintHelper } from './mint'
 import { handleBurn as handleBurnHelper } from './burn'
 import { handleSwap as handleSwapHelper } from './swap'
-import { handleFlash as handleFlashHelper } from './flash'
 import { handleCollect as handleCollectHelper } from './collect'
-export const handleInitialize = handleInitializeHelper
-export const handleMint = handleMintHelper
-export const handleBurn = handleBurnHelper
-export const handleSwap = handleSwapHelper
-export const handleFlash = handleFlashHelper
-export const handleCollect = handleCollectHelper
+import { handleFlash as handleFlashHelper } from './flash'
+import { Initialize, Mint, Burn, Swap, Collect, Flash } from '../../types/templates/Pool/Pool'
+
+// Workaround for limited export types in Assemblyscript.
+export function handleInitialize(event: Initialize): void {
+  handleInitializeHelper(event)
+}
+export function handleMint(event: Mint): void {
+  handleMintHelper(event)
+}
+export function handleBurn(event: Burn): void {
+  handleBurnHelper(event)
+}
+export function handleSwap(event: Swap): void {
+  handleSwapHelper(event)
+}
+export function handleCollect(event: Collect): void {
+  handleCollectHelper(event)
+}
+export function handleFlash(event: Flash): void {
+  handleFlashHelper(event)
+}
