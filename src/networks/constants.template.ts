00e3cfb15b8d9365adc0d1b9c54515c3397c6f7b
Ian Lapham <ianlapham@gmail.com>
2022-12-08 11:14:01 -0500

    start refactor

--- src/networks/constants.template.ts
+++ src/networks/constants.template.ts
@@ -4,8 +4,6 @@ import { BigDecimal } from '@graphprotocol/graph-ts'
 export let FACTORY_ADDRESS = '{{factory_address}}'
 export let WETH_ADDRESS = '{{weth_address}}'
 
-
-
 // tokens where USD value is safe to use for globals
 export let WHITELIST_TOKENS: string[] = [
   {{#whitelist_tokens}}
@@ -21,13 +19,13 @@ export let STABLE_COINS: string[] = [
 ]
 
 // used for safe eth pricing 
-export let STABLE_POOL_ADDRESS = '{{stable_pool_address}}'
+export const STABLE_POOL_ADDRESS = '{{stable_pool_address}}'
 
 // determines which token to use for eth<-> rate, true means stable is token0 in pool above 
-export let STABLE_IS_TOKEN_0 = {{stableIsToken0}}
+export const STABLE_IS_TOKEN_0 = {{stableIsToken0}}
 
 // minimum eth required in pool to count usd values towards global prices 
-export let MINIMUM_ETH_LOCKED = BigDecimal.fromString('{{min_eth}}')
+export const MINIMUM_ETH_LOCKED = BigDecimal.fromString('{{min_eth}}')
 
 // pool that breaks with subgraph logic 
-export let ERROR_POOL = '{{error_pool}}'
+export const ERROR_POOL = '{{error_pool}}'
