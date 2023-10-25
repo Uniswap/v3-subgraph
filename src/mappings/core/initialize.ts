3c413da417c55570bf77e91feb56199ad4300386
mike-data-nexus <112513079+mike-data-nexus@users.noreply.github.com>
2023-10-25 16:24:32 -0400

    Split mapping files into individual files

---
+++ src/mappings/core/initialize.ts
@@ -0,0 +1,34 @@
+import { Bundle, Pool, Token } from '../../types/schema'
+import { BigInt } from '@graphprotocol/graph-ts'
+import { Initialize } from '../../types/templates/Pool/Pool'
+import { findEthPerToken, getEthPriceInUSD } from '../../utils/pricing'
+import {
+  updatePoolDayData,
+  updatePoolHourData,
+} from '../../utils/intervalUpdates'
+
+export function handleInitialize(event: Initialize): void {
+  // update pool sqrt price and tick
+  let pool = Pool.load(event.address.toHexString())
+  pool.sqrtPrice = event.params.sqrtPriceX96
+  pool.tick = BigInt.fromI32(event.params.tick)
+  pool.save()
+  
+  // update token prices
+  let token0 = Token.load(pool.token0)
+  let token1 = Token.load(pool.token1)
+
+  // update ETH price now that prices could have changed
+  let bundle = Bundle.load('1')
+  bundle.ethPriceUSD = getEthPriceInUSD()
+  bundle.save()
+
+  updatePoolDayData(event)
+  updatePoolHourData(event)
+
+  // update token prices
+  token0.derivedETH = findEthPerToken(token0 as Token)
+  token1.derivedETH = findEthPerToken(token1 as Token)
+  token0.save()
+  token1.save()
+}
