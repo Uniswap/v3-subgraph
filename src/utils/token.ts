63c6fdbeda02dbb80aa0abe2825095ba2e15b9ca
Data-Nexus <derek.m.meyer@gmail.com>
2024-01-26 18:47:07 -0500

    Updating ABI internal type

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -75,7 +75,7 @@ export function fetchTokenTotalSupply(tokenAddress: Address): BigInt {
 export function fetchTokenDecimals(tokenAddress: Address): BigInt {
   let contract = ERC20.bind(tokenAddress)
   // try types uint8 for decimals
-  let decimalValue = 18
+  let decimalValue = BigInt.fromI32(18)
   let decimalResult = contract.try_decimals()
 
   if (!decimalResult.reverted) {
@@ -88,5 +88,5 @@ export function fetchTokenDecimals(tokenAddress: Address): BigInt {
     }
   }
 
-  return BigInt.fromI32(decimalValue)
+  return decimalValue
 }
