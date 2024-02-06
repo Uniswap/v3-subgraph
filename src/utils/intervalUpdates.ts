73d419f048b44682400c5e3cb87cc1c8222bd0f4
Data-Nexus <derek.m.meyer@gmail.com>
2024-02-06 11:52:44 -0500

    Fixed nullable load

--- src/utils/intervalUpdates.ts
+++ src/utils/intervalUpdates.ts
@@ -49,7 +49,7 @@ export function updatePoolDayData(event: ethereum.Event): PoolDayData {
     .concat('-')
     .concat(dayID.toString())
   let pool = Pool.load(event.address.toHexString())!
-  let poolDayData = PoolDayData.load(dayPoolID)!
+  let poolDayData = PoolDayData.load(dayPoolID)
   if (poolDayData === null) {
     poolDayData = new PoolDayData(dayPoolID)
     poolDayData.date = dayStartTimestamp
