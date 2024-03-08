d5b61bf88dcb6cf379f787323a46bbc6b17425fb
Michael Wang <44713145+mzywang@users.noreply.github.com>
2024-03-08 10:24:09 -0500

    update for blast-mainnet

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
 import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984'
+export const FACTORY_ADDRESS = '0x792edAdE80af5fC680d96a2eD80A44247D2Cf6Fd'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
