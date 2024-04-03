a55e91412f5642f8da3b1e24716e3fdd6ba33dea
Michael Wang <44713145+mzywang@users.noreply.github.com>
2024-04-03 15:58:04 -0400

    update export

--- src/mappings/pool/index.ts
+++ src/mappings/pool/index.ts
@@ -1,8 +1,6 @@
-import { handleBurn as handleBurnHelper } from './burn'
-import { handleInitialize as handleInitializeHelper } from './initialize'
-import { handleMint as handleMintHelper } from './mint'
-import { handleSwap as handleSwapHelper } from './swap'
-export const handleInitialize = handleInitializeHelper
-export const handleMint = handleMintHelper
-export const handleBurn = handleBurnHelper
-export const handleSwap = handleSwapHelper
+import { handleBurn } from './burn'
+import { handleInitialize } from './initialize'
+import { handleMint } from './mint'
+import { handleSwap } from './swap'
+
+export { handleInitialize, handleMint, handleBurn, handleSwap }
