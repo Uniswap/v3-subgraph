2c1a9a169582ac9e315b23002bf199190e76155b
Michael Wang <44713145+mzywang@users.noreply.github.com>
2024-04-24 17:38:41 -0400

    update network to csv

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 import { Factory as FactoryContract } from '../types/templates/Pool/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x740b1c1de25031C31FF4fC9A62f554A55cdC1baD'
+export const FACTORY_ADDRESS = '0xdB1d10011AD0Ff90774D0C6Bb92e5C5c8b4461F7'
 
 export const ZERO_BI = BigInt.fromI32(0)
 export const ONE_BI = BigInt.fromI32(1)
--- src/utils/pricing.ts
+++ src/utils/pricing.ts
@@ -4,40 +4,20 @@ import { exponentToBigDecimal, safeDiv } from '../utils/index'
 import { Bundle, Pool, Token } from './../types/schema'
 import { ONE_BD, ZERO_BD, ZERO_BI } from './constants'
 
-export const WAVAX_ADDRESS = '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7'
-export const WAVAX_USDC_05_POOL = '0xfae3f424a0a47706811521e3ee268f00cfb5c45e'
-export const STABLECOIN_IS_TOKEN0 = false
+export const WBNB_ADDRESS = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
+export const USDC_WBNB_03_POOL = '0x6fe9e9de56356f7edbfcbb29fab7cd69471a4869'
+export const STABLECOIN_IS_TOKEN0 = true
 
-const DAI_E_ADDRESS = '0xd586e7f844cea2f87f50152665bcbc2c279d8d70'
-const DAI_ADDRESS = '0xba7deebbfc5fa1100fb055a87773e1e99cd3507a'
-const USDC_E_ADDRESS = '0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664'
-const USDC_ADDRESS = '0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e'
-const USDT_E_ADDRESS = '0xc7198437980c041c805a1edcba50c1ce5db95118'
-const USDT_ADDRESS = '0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7'
+const USDT_ADDRESS = '0x55d398326f99059ff775485246999027b3197955'
+const USDC_ADDRESS = '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d'
 
 // token where amounts should contribute to tracked volume and liquidity
 // usually tokens that many tokens are paired with s
-export const WHITELIST_TOKENS: string[] = [
-  WAVAX_ADDRESS,
-  DAI_E_ADDRESS,
-  DAI_ADDRESS,
-  USDC_E_ADDRESS,
-  USDC_ADDRESS,
-  USDT_E_ADDRESS,
-  USDT_ADDRESS,
-  '0x130966628846bfd36ff31a822705796e8cb8c18d', // mim
-]
+export const WHITELIST_TOKENS: string[] = [WBNB_ADDRESS, USDT_ADDRESS, USDC_ADDRESS]
 
-export const STABLE_COINS: string[] = [
-  DAI_E_ADDRESS,
-  DAI_ADDRESS,
-  USDC_E_ADDRESS,
-  USDC_ADDRESS,
-  USDT_E_ADDRESS,
-  USDT_ADDRESS,
-]
+export const STABLE_COINS: string[] = [USDT_ADDRESS, USDC_ADDRESS]
 
-export const MINIMUM_ETH_LOCKED = BigDecimal.fromString('1000')
+export const MINIMUM_ETH_LOCKED = BigDecimal.fromString('100')
 
 const Q192 = BigInt.fromI32(2).pow(192 as u8)
 export function sqrtPriceX96ToTokenPrices(sqrtPriceX96: BigInt, token0: Token, token1: Token): BigDecimal[] {
@@ -53,7 +33,7 @@ export function sqrtPriceX96ToTokenPrices(sqrtPriceX96: BigInt, token0: Token, t
 }
 
 export function getEthPriceInUSD(
-  stablecoinWrappedNativePoolAddress: string = WAVAX_USDC_05_POOL,
+  stablecoinWrappedNativePoolAddress: string = USDC_WBNB_03_POOL,
   stablecoinIsToken0: boolean = STABLECOIN_IS_TOKEN0, // true is stablecoin is token0, false if stablecoin is token1
 ): BigDecimal {
   const stablecoinWrappedNativePool = Pool.load(stablecoinWrappedNativePoolAddress)
@@ -70,7 +50,7 @@ export function getEthPriceInUSD(
  **/
 export function findEthPerToken(
   token: Token,
-  wrappedNativeAddress: string = WAVAX_ADDRESS,
+  wrappedNativeAddress: string = WBNB_ADDRESS,
   stablecoinAddresses: string[] = STABLE_COINS,
   minimumEthLocked: BigDecimal = MINIMUM_ETH_LOCKED,
 ): BigDecimal {
